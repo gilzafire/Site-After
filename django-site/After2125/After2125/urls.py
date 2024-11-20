b912c94f1fe48d9dfa1a02759f9c6e8f23517348
gilzafire <gilza.ab@gmail.com>
2024-11-20 08:48:04 +0100

    final kube

--- django-site/After2125/After2125/urls.py
+++ django-site/After2125/After2125/urls.py
@@ -23,5 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('public/', include('public.urls')),
+    path('', include('public.urls')),
 ] 
